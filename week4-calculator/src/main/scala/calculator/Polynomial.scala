df63c8c873a35a8da0c0b3c6d582ab886e8c4f1c
Jake Bishop <jake.bishop@geekmode.com>
2016-09-11 00:31:15 -0500

    week 4 assignment, completed tweet length and polynomial solutions.
    Signed-off-by: Jake Bishop <jake.bishop@geekmode.com>

---
+++ week4-calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,22 @@
+package calculator
+
+import scala.math.sqrt
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+                   c: Signal[Double]): Signal[Double] = {
+    Signal(b() * b() - 4 * a() * c())
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal {
+      if (a() == 0) Set()
+      else delta() match {
+        case d if d > 0 => Set((-b()) + sqrt(d) / (a() * 2), (-b()) - sqrt(d) / (a() * 2))
+        case d if d == 0 => Set((-b()) / (a() * 2))
+        case default => Set()
+      }
+    }
+  }
+}
